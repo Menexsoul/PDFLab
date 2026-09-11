d14964856d4b7dc8a268f7144316f0cda46711d0
Menexsoul <thomas.debroize30@gmail.com>
2026-09-11 15:34:42 +0200

    configuration Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import eslintConfigPrettier from "eslint-config-prettier";
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -14,6 +15,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
